d2b2dfee7a7e836562149301ccd608f2c3f59426
Willian Paixao <willian@ufpa.br>
2015-05-25 14:48:51 -0300

    More documentation.

--- README.md
+++ README.md
@@ -13,6 +13,12 @@ pip install django
 pip install fabric
 ```
 
+Don't forget to initialize your SQLite database, as in any Django project:
+
+```
+python manage.py syncdb
+```
+
 ## Basic usage
 By now, you haven't install *farinha* as a Python module, the commands aren't in
 your PATH yet. But you can run them like this:
@@ -20,4 +26,21 @@ your PATH yet. But you can run them like this:
 ```
 python manage.py ls
 python manage.py cat 1
+python manage.py echo 1
+```
+
+
+## Basic commands
+### *add* a new snippet
+Creates a new snippet with the given title.
+
+#### Examples
+```
+python manage.py add script.sh
+python manage.py add first second
+```
+
+For further information, type:
+```
+python manage.py help add
 ```
--- farinha/management/commands/echo.py
+++ farinha/management/commands/echo.py
@@ -1,17 +1,23 @@
+"""Print a given snippet on the standard output."""
+
 from django.core.management.base import BaseCommand, CommandError
 
 from farinha.models import Snippet
 
 
 class Command(BaseCommand):
+
+    """Print a given snippet on the standard output."""
+
     args = "<snippet_id snippet_id ...>"
     help = "Shows snippet's contents with the given ids."
 
     def handle(self, *args, **options):
+        """Receive the snippet's titles and store them in the database."""
         if args:
-            for snippet_id in args:
+            for s in args:
                 try:
-                    s = Snippet.objects.get(pk=int(snippet_id))
+                    s = Snippet.objects.get(pk=int(s))
                     if s.header is not None:
                         self.stdout.write(s.header)
                     if s.body is not None:
@@ -19,4 +25,4 @@ class Command(BaseCommand):
                     if s.footer is not None:
                         self.stdout.write(s.footer)
                 except Snippet.DoesNotExist:
-                    raise CommandError("Snippet %s doesn't exist." % snippet_id)
+                    raise CommandError("Snippet %s doesn't exist." % s)
