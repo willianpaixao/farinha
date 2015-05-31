33f0a514492b57b437c0c00b2e5d34ffa00c3f91
Willian Paixao <willian@ufpa.br>
2015-05-31 01:00:03 -0300

    edit feature added.
    Now the add function can read from and store the file to the databases. Cool!

--- farinha/management/commands/add.py
+++ farinha/management/commands/add.py
@@ -1,4 +1,4 @@
-"""Add an empty Snippet."""
+"""Add an empty snippet."""
 
 from django.core.management.base import BaseCommand, CommandError
 
@@ -7,21 +7,41 @@ from farinha.models import Snippet
 
 class Command(BaseCommand):
 
-    """Creates an empty Snippet with the given title.
+    """Creates an empty snippet with the given title.
 
     Args:
-        args (str): List of Snippets to be created.
+        args (str): Snippet to be created.
     """
 
-    args = "<snippet_id snippet_id ...>"
-    help = "Creates a empty Snippets with the given titles."
+    args = "<snippet_id>"
+    help = "Creates a empty snippet with the given title."
+
+    def add_arguments(self, parser):
+        """Add optional arguments."""
+        parser.add_argument('--header', nargs='?', type=open)
+        parser.add_argument('--body', nargs='?', type=open)
+        parser.add_argument('--footer', nargs='?', type=open)
 
     def handle(self, *args, **options):
-        """Iterate throw the list to create the Snippets."""
+        """Iterate throw the list to create the snippets."""
         if args:
-            for t in args:
-                s = Snippet(title=t)
-                s.save()
+            s = Snippet(title=args[0])
+            if options['header']:
+                s.header = options['header'].read()
+#            else:
+#                with open('farinha/header.txt', 'r') as f:
+#                    s.header = f.read()
+            if options['body']:
+                s.body = options['body'].read()
+#            else:
+#                with open('farinha/body.txt', 'r') as f:
+#                    s.body = f.read()
+            if options['footer']:
+                s.footer = options['footer'].read()
+#            else:
+#                with open('farinha/footer.txt', 'r') as f:
+#                    s.footer = f.read()
+            s.save()
         else:
-            self.stdout.write("You need to provide at least the Snippet's \
+            self.stdout.write("You need to provide at least the snippet's \
                 title.")
