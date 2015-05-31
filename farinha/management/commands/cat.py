e6341f598356e61ca85387cb3a06f34c420d454c
Willian Paixao <willian@ufpa.br>
2015-05-31 10:41:48 -0300

    bug fix on cat function

--- farinha/management/commands/cat.py
+++ farinha/management/commands/cat.py
@@ -1,21 +1,34 @@
+"""Export the snippet to a file."""
+
 from django.core.management.base import BaseCommand, CommandError
 
 from tempfile import NamedTemporaryFile
 
 from farinha.models import Snippet
 
+
 class Command(BaseCommand):
+
+    """Export the snippet to a file."""
+
     args = "<snippet_id snippet_id ...>"
     help = "Shows snippet's contents with the given ids."
 
     def handle(self, *args, **options):
+        """Receive snippe id list and export to files."""
         if args:
             for snippet_id in args:
                 with NamedTemporaryFile(delete=False) as f:
                     try:
                         s = Snippet.objects.get(pk=int(snippet_id))
-                        f.write(s.header)
-                        f.write(s.body)
-                        f.write(s.footer)
+                        if s.header:
+                            f.write(s.header)
+                        if s.body:
+                            f.write(s.body)
+                        if s.footer:
+                            f.write(s.footer)
+                        self.stdout.write(f.name)
                     except Snippet.DoesNotExist:
-                        raise CommandError("Snippet %s doesn't exist." % snippet_id)
+                        raise CommandError(
+                                "Snippet %s doesn't exist." %
+                                snippet_id)
